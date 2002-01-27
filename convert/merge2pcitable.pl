84809efc86bbc8b3c9cb0a5b99744ae51c87e90a
prigaux <prigaux@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2002-01-27 22:48:00 +0000

    we now want yenta_socket
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/ldetect-lst/trunk@39602 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- convert/merge2pcitable.pl
+++ convert/merge2pcitable.pl
@@ -69,7 +69,7 @@ sub read_pcitable {
 	    $module = "unknown" if $id1 == 0x1011 && (0x0024 <= $id2 && $id2 <= 0x0025); 
 	      # known errors in redhat's pcitable
 	      # these are pci to pci bridge
-	    $module = "i82365" if $module =~ /yenta_socket/;
+	    $module = "yenta_socket" if $module =~ /i82365/;
 	    my $id = join '', map { s/^0x//; $_ } $id1, $id2, $subid1, $subid2;
 	    $drivers{$id} and print STDERR "$f $line: multiple entry for $id (skipping $module $text)\n";
 	    $drivers{$id} ||= [ map &$rm_quote, $module, $text ];
